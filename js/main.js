3b6b42c2d9f9bf973bdbfb9a1417a999f95b474e
MCAlagaesia <contact@mcalagaesia.com>
2019-03-04 22:02:12 +1100

    Initial commit of prior work

---
+++ js/main.js
@@ -0,0 +1,84 @@
+(function($) {
+  
+  "use strict";
+
+    /* 
+   wow js
+   ========================================================================== */
+    //Initiat WOW JS
+    new WOW().init();
+
+    $(window).on('load', function() {
+
+        $(window).on('scroll', function() {
+            if ($(window).scrollTop() > 200) {
+                $('.fixed-top').addClass('menu-bg');
+            } else {
+                $('.fixed-top').removeClass('menu-bg');
+            }
+        });
+
+    });
+
+  /* 
+   SMOOTH SCROLL
+   ========================================================================== */
+    var scrollAnimationTime = 1200,
+        scrollAnimation = 'easeInOutExpo';
+
+    $('a.scrollto').on('bind', 'click.smoothscroll', function (event) {
+        event.preventDefault();
+        var target = this.hash;
+        
+        $('html, body').stop().animate({
+            'scrollTop': $(target).offset().top
+        }, scrollAnimationTime, scrollAnimation, function () {
+            window.location.hash = target;
+        });
+    });
+
+/* 
+   Back Top Link
+   ========================================================================== */
+    var offset = 200;
+    var duration = 500;
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > offset) {
+        $('.back-to-top').fadeIn(400);
+      } else {
+        $('.back-to-top').fadeOut(400);
+      }
+    });
+
+    $('.back-to-top').on('click',function(event) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: 0
+      }, 600);
+      return false;
+    })
+	
+	$('.scroll-down').on('click',function(event) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: $("#game").offset().top
+      }, 600);
+      return false;
+    })
+
+
+/* stellar js
+  ========================================================*/
+  $.stellar({
+    horizontalScrolling: true,
+    verticalOffset: 40,
+    responsive: true
+  });
+
+/* 
+   Page Loader
+   ========================================================================== */
+  $('#loader').fadeOut();
+
+}(jQuery));
+
